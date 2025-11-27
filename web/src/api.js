d41f40f1cb749261b195b9df190b1351cd4c6d2f
Adriano Frongillo <frongillo.adriano@gmail.com>
2025-11-27 22:28:57 +0100

    Fix: Add Authorization header to API requests via interceptor

--- web/src/api.js
+++ web/src/api.js
@@ -6,6 +6,15 @@ const api = axios.create({
     baseURL: API_URL,
 });
 
+// Add token to all requests
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
 export const getNotes = () => api.get('/notes');
 export const createNote = (note) => api.post('/notes', note);
 export const updateNote = (id, note) => api.put(`/notes/${id}`, note);
